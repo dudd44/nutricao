3bd9d62df69a8ccd9361f5655c17fa82cdb45cc3
dudd44 <luciano.eduarda@escola.pr.gov.br>
2025-10-07 09:00:44 -0300

    bvbv

--- js/principal.js
+++ js/principal.js
@@ -3,3 +3,16 @@ console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("Eduarda Nutricionista");
    
+             var paciente = document.querySelector("#primeiro-paciente");
+            console.log(paciente);
+
+            var tdPeso = paciente.querySelector(".info-peso");
+            console.log(tdPeso);
+
+            var peso = tdPeso.textContent;
+            console.log(paciente)
+            console.log(peso)
+
+       var tdAltura = document.querySeclector(".info-altura");
+       var altura = tdAltura.textContent;
+       console.log(altura);    
