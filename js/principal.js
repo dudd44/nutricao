7638586856c895cfb88cc7906ce4c012db6b0a37
dudd44 <luciano.eduarda@escola.pr.gov.br>
2025-10-10 11:05:11 -0300

    c czx

--- js/principal.js
+++ js/principal.js
@@ -22,13 +22,13 @@ titulo.textContent = ("Eduarda Nutricionista");
        var pesoEhValido = true;
        var alturaEhValida =  true;
 
-       if (peso <0 || peso > 1000){
+       if (peso <=0 || peso >= 1000){
         console.log("Peso inválido!");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido!"; 
        }
 
-       if(altura < 0 || altura > 3.00){
+       if(altura <= 0 || altura >= 3.00){
         console.log("Altura inválida!");
         alturaEhValida = false;
         tdImc.textContent = "Altura iválida!"; 
