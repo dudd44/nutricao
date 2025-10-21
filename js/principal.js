4227b172289a51f0e81ef501540aaedbb5f41cb9
dudd44 <luciano.eduarda@escola.pr.gov.br>
2025-10-21 09:40:04 -0300

    2

--- js/principal.js
+++ js/principal.js
@@ -2,41 +2,43 @@ var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("Eduarda Nutricionista");
-   
-        var paciente = document.querySelectorAll(".paciente");
 
-        for (var i = 0; i <pacientes.length;i++) {
-                console.log( pacientes[i]);
-        }
+var paciente = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+        var paciente = paciente[i];
 
         var tdPeso = paciente.querySelector(".info-peso");
         var peso = tdPeso.textContent;
 
-       var tdAltura = paciente.querySelector(".info-altura");
-       var altura = tdAltura.textContent;  
-       
-       var tdImc = paciente.querySelector(".info-imc");
+        var tdAltura = paciente.querySelector(".info-altura");
+        var altura = tdAltura.textContent;
 
-       var pesoEhValido = true;
-       var alturaEhValida =  true;
+        var tdImc = paciente.querySelector(".info-imc");
 
-       if (peso <= 0 || peso >= 1000){
-        console.log("Peso inválido!");
-        pesoEhValido = false;
-        tdImc.textContent = "Peso inválido!"; 
-       }
+        var pesoEhValido = true;
+        var alturaEhValida = true;
 
-       if(altura <= 0 || altura >= 3.00){
-        console.log("Altura inválida!");
-        alturaEhValida = false;
-        tdImc.textContent = "Altura iválida!"; 
-       }
+        if (peso <= 0 || peso >= 1000) {
+                console.log("Peso inválido!");
+                pesoEhValido = false;
+                tdImc.textContent = "Peso inválido!";
+        }
 
-       if (alturaEhValida && pesoEhValido){
-        var imc = peso / (altura*altura);
-        tdImc.textContent = imc; 
-       }
+        if (altura <= 0 || altura >= 3.00) {
+                console.log("Altura inválida!");
+                alturaEhValida = false;
+                tdImc.textContent = "Altura iválida!";
+        }
 
-       
+        if (alturaEhValida && pesoEhValido) {
+                var imc = peso / (altura * altura);
+                tdImc.textContent = imc;
+        }
 
-       
+}
+
+
+
+
